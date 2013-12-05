ea4362f2aa622e4522bed3dd9078e48ffca4a5ca
Yasir <yasir.ghunaim@gmail.com>
2013-12-05 16:59:50 -0500

    Implemented login to Scholar

--- project.py
+++ project.py
@@ -2,27 +2,34 @@ import urllib
 import urllib2
 import cookielib
 import sys
-url = "https://auth.vt.edu/login?service=https%3A%2F%2Fwebapps.banner.vt.edu%2Fbanner-cas-prod%2Fauthorized%2Fbanner%2FSelfService%3Bjsessionid%3D21709F288B978AFC233DEA9658850C04.mt-prod-1"
 
-#Enable cookies 
-jar = cookielib.FileCookieJar("cookies")
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
+def login(username, password):
+	url = "https://auth.vt.edu/login?service=https%3A%2F%2Fscholar.vt.edu%2Fsakai-login-tool%2Fcontainer"
 
-#HTTP GET to grab lt parameter that is used in logging in
-response = opener.open(url).read()
-#extract lt
-lt = response.split("\"lt\" value=\"")[1].split("\"")[0]
-#grab cookie from response
+	#Enable cookies 
+	jar = cookielib.FileCookieJar("cookies")
+	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
+
+	#HTTP GET to grab lt parameter that is used in logging in
+	response = opener.open(url).read()
+	#extract lt
+	lt = response.split("\"lt\" value=\"")[1].split("\"")[0]
+	#grab cookie from response
+
+	data = urllib.urlencode({'username' : str(username).strip(),
+							 'password' : str(password).strip(),
+							 'lt' : lt,
+							 'execution' : 'e1s1',
+							 '_eventId' : 'submit',
+							 'submit' : '_submit'})
+	response = opener.open(url,data)
+	if not response.info().getheader('Set-Cookie'):
+		print("Invalid username or password")
+	else:
+		print("Login successful")
+
 
 loginInfo = sys.stdin.readline()
 username = loginInfo.split(" ")[0]
 password = loginInfo.split(" ")[1]
-
-data = urllib.urlencode({'username' : username,
-                         'password' : password,
-                         'lt' : lt,
-                         'execution' : 'e1s1',
-                         '_eventId' : 'submit',
-                         'submit' : '_submit'})
-response = urllib2.urlopen(url, data).read()
-print(response)
+login(username, password)
