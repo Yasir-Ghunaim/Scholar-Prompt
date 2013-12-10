8f05e77f8fea5ccbf0ad7f62debd36d557f2bb48
Yasir <yasir.ghunaim@gmail.com>
2013-12-10 11:43:48 -0500

    Changed implementation to Mechanize

--- project.py
+++ project.py
@@ -1,56 +1,40 @@
-import urllib
-import urllib2
-import cookielib
+import mechanize
 import sys
+import getpass
 
-#Given a username and a password this function will attempt to login in to Scholar
-def login(opener, jar, username, password):
+browser = mechanize.Browser()
+
+def login(username, password):
 	url = "https://auth.vt.edu/login?service=https%3A%2F%2Fscholar.vt.edu%2Fsakai-login-tool%2Fcontainer"
-
-	#HTTP GET to grab lt parameter that is used in logging in
-	response = opener.open(url).read()
-	#extract lt
-	lt = response.split("\"lt\" value=\"")[1].split("\"")[0]
-	#grab cookie from response
-
-	data = urllib.urlencode({'username' : str(username).strip(),
-							 'password' : str(password).strip(),
-							 'lt' : lt,
-							 'execution' : 'e1s1',
-							 '_eventId' : 'submit',
-							 'submit' : '_submit'})
-	response = opener.open(url,data)
-	if response.info().getheader('Set-Cookie'):
+	browser.open(url)
+	browser.select_form(nr = 0)
+	browser.form['username'] = username
+	browser.form['password'] = password
+	browser.submit()
+	if browser.response().info().getheader('Set-Cookie'):
 		#login is successful when cookie is set
 		print("Login successful")
 		return True
 	else:
 		#cookie will be empty when login fail 
-		print("Invalid username or password\n")
+		print("Invalid username or password")
 		return False
+
 
 def main():
 	isLogged = False
 	
 	while not isLogged:
-		#Enable cookies
-		global jar, opener 
-		jar = cookielib.FileCookieJar("cookies")
-		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
-		
 		#get username and password from user and attempt to login
 		print("Enter username and password: ")
-		loginInfo = sys.stdin.readline()
-		username = loginInfo.split(" ")[0]
-		password = loginInfo.split(" ")[1]
-		isLogged = login(opener, jar, username, password)
+		print "Username: ",
+		username = str.strip(sys.stdin.readline())
+		password = getpass.getpass()
+		isLogged = login(username, password)
 	
-	
-	
+	#if isLogged:
+	#	print browser.response().read();
 	
 
-#This is python style to call main function
-#basically when program start the defined varible __name__
-#will hold the value "__main__" in which case we call our function main
 if __name__ ==  "__main__":
 	main()
