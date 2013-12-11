e4149cc04bddc7c70d95919bfeb9cd83e30d9d18
Yasir <yasir.ghunaim@gmail.com>
2013-12-11 11:01:27 -0500

    added shortcuts

--- project.py
+++ project.py
@@ -16,10 +16,12 @@ class Example(QtGui.QWidget):
         self.openBtn = QtGui.QPushButton('Open', self)
         self.openBtn.move(10, 10)
         self.openBtn.clicked.connect(self.openFileButton)
+        QtGui.QShortcut(QtGui.QKeySequence("Ctrl+o"), self, self.openFileButton)
         # save button
         self.saveBtn = QtGui.QPushButton('Save', self)
         self.saveBtn.move(310, 10)
-        #self.saveBtn.clicked.connect(self.openFile)
+        self.saveBtn.clicked.connect(self.saveFileButton)
+        QtGui.QShortcut(QtGui.QKeySequence("Ctrl+s"), self, self.saveFileButton)
         # input text edit
         self.inputTextEdit = QtGui.QTextEdit(self)
         self.inputTextEdit.move(10, 40)
@@ -32,14 +34,16 @@ class Example(QtGui.QWidget):
         self.openBtn.clicked.connect(self.convertToButton)
         # convert to combo box
         self.convertToCombo = QtGui.QComboBox(self)
-        self.convertToCombo.addItem("Binary to ASCII")
         self.convertToCombo.addItem("ASCII to Binary")
-        self.convertToCombo.addItem("Hex to Binary")
-        self.convertToCombo.addItem("Binary to Hex")
+        self.convertToCombo.addItem("Binary to ASCII")
         self.convertToCombo.addItem("ASCII to Hex")
         self.convertToCombo.addItem("Hex to ASCII")
+        self.convertToCombo.addItem("Binary to Hex")
+        self.convertToCombo.addItem("Hex to Binary")
         self.convertToCombo.move(20, 240)
         
+        #close program
+        QtGui.QShortcut(QtGui.QKeySequence("Ctrl+q"), self, self.close)
         self.setGeometry(100, 100, 600, 400)
         self.setWindowTitle('Input dialog')
         self.show()
@@ -51,7 +55,14 @@ class Example(QtGui.QWidget):
         #read file and show its content in the input field
         self.converter.newFile(fileName)
         self.inputTextEdit.setText(self.converter.data)
-        
+    def saveFileButton(self):
+		fileName = QtGui.QFileDialog.getSaveFileName(self, "Save file", "", ".txt")
+		try:
+			f = open(fileName,'w')
+			f.write(str(self.outputTextEdit.toPlainText()))
+			f.close()
+		except:
+			print 'invalid filename'
     def convertToButton(self):
         self.converter.data = str(self.inputTextEdit.toPlainText())
         convertType = str(self.convertToCombo.currentText())
